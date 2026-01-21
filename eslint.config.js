60563191c66d13b8b0a286ad65f64513dc567778
Anton Nakanechny <kashot.kukuskin@gmail.com>
2026-01-21 16:05:51 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -22,10 +22,14 @@ export default defineConfig(
 			globals: { ...globals.browser, ...globals.node }
 		},
 		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 			'no-undef': 'off',
-			// 'no-unuseed-vars': ['error', { argsIgnorePattern: '^_' }]
+			'@typescript-eslint/consistent-type-imports': [
+				'error',
+				{ prefer: 'type-imports', fixStyle: 'inline-type-imports' }
+			],
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			'semi': ['error', 'always'],
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 		}
 	},
 	{
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
-import { mount } from 'svelte';
 import { App } from '@/components/app';
-import { init, isTMA, retrieveLaunchParams, initData } from '@tma.js/sdk-svelte';
+import { init, isTMA } from '@tma.js/sdk-svelte';
+import { mount } from 'svelte';
 
 import './index.css';
-
-if (import.meta.env.VITE_USE_MOCKS === 'enabled') {
-  await import('./mocks/tg');
-}
 
 if (isTMA()) {
   init();
 }
-
-const data = retrieveLaunchParams();
-
-console.log('tgWebAppData:', data.tgWebAppData?.hash);
-console.log(initData);
-
 
 const app = mount(App, {
   target: document.getElementById('app')!
